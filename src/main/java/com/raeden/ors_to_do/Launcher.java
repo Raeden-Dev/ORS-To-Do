d19161ae3acef9c0b9ffb7742f9f76a9a5d3358d
Raeden <raider2891@gmail.com>
2026-03-31 23:04:50 +0600

    Application Implement

--- src/main/java/com/raeden/ors_to_do/Launcher.java
+++ src/main/java/com/raeden/ors_to_do/Launcher.java
@@ -2,7 +2,6 @@ package com.raeden.ors_to_do;
 
 public class Launcher {
     public static void main(String[] args) {
-        // This bypasses the strict JavaFX module check
         TaskTrackerFXApp.main(args);
     }
 }
